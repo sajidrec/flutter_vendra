004b47cf1f45f50f2b60cf55256e7e7290fa1cb2
sajid <sajidrec@gmail.com>
2025-10-14 09:45:02 +0600

    - add business information page

--- lib/app/modules/become_seller_page/presentation/controller/become_seller_step_page_controller.dart
+++ lib/app/modules/become_seller_page/presentation/controller/become_seller_step_page_controller.dart
@@ -15,9 +15,15 @@ class BecomeSellerStepPageController extends GetxController {
   final TextEditingController cityTec = TextEditingController();
 
   final TextEditingController searchStateTec = TextEditingController();
+  final TextEditingController searchCountryTec = TextEditingController();
 
   late final List<String> stateList;
   Fuzzy? fuse;
+
+  late final List<Map<String, dynamic>> countryList;
+  List<Map<String, dynamic>> filteredCountryList = [];
+  Fuzzy? countryFuse;
+  Map<String, dynamic>? selectedCountry;
 
   List<String> filteredStateList = [];
   String? selectedState;
@@ -31,6 +37,7 @@ class BecomeSellerStepPageController extends GetxController {
   void onInit() {
     super.onInit();
     loadStates();
+    loadCountries();
   }
 
   void increaseProgressIndex() {
@@ -99,6 +106,47 @@ class BecomeSellerStepPageController extends GetxController {
     return selectedState == state;
   }
 
+
+  Future<void> loadCountries() async {
+    final String response = await rootBundle.loadString(
+      'lib/app/core/data/datasources/country_info.json',
+    );
+    final List<dynamic> data = json.decode(response);
+    countryList = data.map((e) => Map<String, dynamic>.from(e)).toList();
+    filteredCountryList = List.from(countryList);
+
+    countryFuse = Fuzzy(
+      countryList.map((e) => e['name']).toList(),
+      options: FuzzyOptions(threshold: 0.5),
+    );
+    update();
+  }
+
+  void searchCountries(String query) {
+    if (countryFuse == null) return;
+    if (query.isEmpty) {
+      filteredCountryList = List.from(countryList);
+    } else {
+      final results = countryFuse!.search(query);
+      filteredCountryList = results
+          .map((r) => countryList.firstWhere((c) => c['name'] == r.item))
+          .toList();
+    }
+    update();
+  }
+
+  void selectCountry(Map<String, dynamic> country) {
+    selectedCountry = (selectedCountry?['name'] == country['name'])
+        ? null
+        : country;
+    update();
+  }
+
+  bool isCountrySelected(Map<String, dynamic> country) {
+    return selectedCountry != null &&
+        selectedCountry!['name'] == country['name'];
+  }
+
   @override
   void onClose() {
     firstNameTec.dispose();
@@ -107,6 +155,7 @@ class BecomeSellerStepPageController extends GetxController {
     businessTypeTec.dispose();
     streetTec.dispose();
     cityTec.dispose();
+    searchCountryTec.dispose();
     super.onClose();
   }
 }
--- lib/app/modules/become_seller_page/presentation/views/become_seller_step_page.dart
+++ lib/app/modules/become_seller_page/presentation/views/become_seller_step_page.dart
@@ -1,3 +1,4 @@
+import 'package:country_flags/country_flags.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
@@ -332,6 +333,16 @@ class BecomeSellerStepPage extends StatelessWidget {
 
               _buildSelectState(controller),
 
+              SizedBox(height: 24.h),
+              Text(
+                "Country/Region",
+                style: TextStyle(
+                  fontSize: 13.sp,
+                  color: AppColors.primaryBlack,
+                ),
+              ),
+              _buildSelectCountry(controller),
+
               SizedBox(height: 36.h),
 
               RichText(
@@ -594,6 +605,214 @@ class BecomeSellerStepPage extends StatelessWidget {
               ),
             ),
             Spacer(),
+            Padding(
+              padding: EdgeInsets.only(right: 14.w),
+              child: SizedBox(
+                width: 20.w,
+                height: 20.h,
+                child: SvgPicture.asset(
+                  AppAssets.forwardArrowIcon,
+                  width: 20.w,
+                  height: 20.h,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  GestureDetector _buildSelectCountry(
+    BecomeSellerStepPageController controller,
+  ) {
+    return GestureDetector(
+      onTap: () {
+        Get.bottomSheet(
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.primaryWhite,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10.r),
+                topRight: Radius.circular(10.r),
+              ),
+            ),
+            child: GetBuilder<BecomeSellerStepPageController>(
+              builder: (controller) {
+                return SizedBox(
+                  width: double.infinity,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 20.h),
+                        Row(
+                          children: [
+                            IconButton(
+                              onPressed: () => Get.back(),
+                              icon: const Icon(Icons.close),
+                            ),
+                            const Spacer(),
+                            Text(
+                              "Country",
+                              style: TextStyle(
+                                fontSize: 17.sp,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            const Spacer(),
+                          ],
+                        ),
+                        SizedBox(height: 16.h),
+
+                        // Search
+                        TextField(
+                          onChanged: (query) {
+                            controller.searchCountries(query);
+                          },
+                          controller: controller.searchCountryTec,
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                              vertical: 13.5.h,
+                              horizontal: 12.5.w,
+                            ),
+                            prefixIcon: const Icon(Icons.search),
+                            hintText: "Search...",
+                            filled: true,
+                            fillColor: AppColors.primaryBlack.withAlpha(
+                              (255 * .06).round(),
+                            ),
+                            border: InputBorder.none,
+                          ),
+                        ),
+
+                        // List of countries
+                        Flexible(
+                          child: ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: controller.filteredCountryList.length,
+                            itemBuilder: (context, index) {
+                              final country =
+                                  controller.filteredCountryList[index];
+                              return GestureDetector(
+                                onTap: () {
+                                  controller.selectCountry(country);
+                                },
+                                child: Padding(
+                                  padding: EdgeInsets.all(14.sp),
+                                  child: Row(
+                                    children: [
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.circular(
+                                          50.r,
+                                        ),
+
+                                        child: SizedBox(
+                                          height: 24.h,
+                                          width: 24.h,
+                                          child: CountryFlag.fromCountryCode(
+                                            country['code'],
+                                          ),
+                                        ),
+                                      ),
+                                      SizedBox(width: 12.w),
+                                      Text(
+                                        country['name'],
+                                        style: TextStyle(fontSize: 17.sp),
+                                      ),
+                                      const Spacer(),
+                                      controller.isCountrySelected(country)
+                                          ? const Icon(Icons.check)
+                                          : const SizedBox.shrink(),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+
+                        Divider(
+                          color: AppColors.primaryBlack.withAlpha(
+                            (255 * .1).round(),
+                          ),
+                        ),
+
+                        // Done button
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: AppColors.primaryBlack,
+                              foregroundColor: AppColors.primaryWhite,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4.r),
+                              ),
+                            ),
+                            onPressed: () => Get.back(),
+                            child: Text(
+                              "Done",
+                              style: TextStyle(fontSize: 17.sp),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20.h),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.primaryWhite,
+          border: Border.all(
+            color: AppColors.primaryBlack.withAlpha((255 * .10).round()),
+            width: 1,
+          ),
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 14.w, vertical: 17.h),
+              child: Row(
+                children: [
+                  if (controller.selectedCountry != null)
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50.r),
+                      child: SizedBox(
+                        width: 24.w,
+                        height: 24.h,
+                        child: CountryFlag.fromCountryCode(
+                          controller.selectedCountry!['code'],
+                          // width: 28.w,
+                          // height: 20.h,
+                          // borderRadius: 4,
+                        ),
+                      ),
+                    ),
+                  if (controller.selectedCountry != null) SizedBox(width: 8.w),
+                  Text(
+                    controller.selectedCountry == null
+                        ? "Select"
+                        : controller.selectedCountry!['name'],
+                    style: TextStyle(
+                      fontSize: 17.sp,
+                      color: controller.selectedCountry == null
+                          ? AppColors.primaryBlack.withAlpha(
+                              (255 * .35).round(),
+                            )
+                          : AppColors.primaryBlack,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(),
             Padding(
               padding: EdgeInsets.only(right: 14.w),
               child: SizedBox(
