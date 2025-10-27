8793f5dd58157801765286843062c8f4f4b8f916
sajid <sajidrec@gmail.com>
2025-10-27 10:33:05 +0600

    - update favorite lots page design

--- lib/app/modules/favorite_page/presentation/views/favorite_page.dart
+++ lib/app/modules/favorite_page/presentation/views/favorite_page.dart
@@ -1,12 +1,8 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:vendra_app/app/core/constants/app_colors.dart';
 import 'package:vendra_app/app/core/data/models/auction_item_model.dart';
-import 'package:get/get.dart';
-
-import '../../../../core/constants/app_assets.dart';
 
 class FavoritePage extends StatelessWidget {
   FavoritePage({super.key});
@@ -28,8 +24,7 @@ class FavoritePage extends StatelessWidget {
       itemName: "Test item",
       isFavourite: false,
       highestBid: 999,
-      description:
-          "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata",
+      description: "Lorem ipsum dolor sit amet, ",
     ),
     AuctionItemModel(
       imgUrl:
@@ -73,88 +68,75 @@ class FavoritePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Favorite"),
+        title: Text("Favorite Lots"),
         centerTitle: true,
         backgroundColor: AppColors.primaryWhite,
       ),
       body: SafeArea(
         child: ListView.separated(
           itemBuilder: (context, index) => Padding(
-            padding: EdgeInsets.symmetric(horizontal: 14.sp, vertical: 6.sp),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(6.r),
-                border: Border.all(
-                  color: AppColors.primaryBlack.withAlpha((255 * .1).round()),
+            padding: EdgeInsets.all(16.sp),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CachedNetworkImage(
+                  imageUrl: auctionItemModelList[index].imgUrl ?? "",
+                  width: 204.w,
+                  height: 204.h,
+                  fit: BoxFit.cover,
                 ),
-              ),
-              child: Padding(
-                padding: EdgeInsets.all(10.sp),
-                child: Stack(
-                  children: [
-                    Column(
+
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 12.sp),
+                    child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(6.r),
-                          child: CachedNetworkImage(
-                            width: double.infinity,
-                            height: Get.width / 1.5,
-                            fit: BoxFit.cover,
-                            imageUrl: auctionItemModelList[index].imgUrl ?? "",
-                            errorWidget: (context, url, error) =>
-                                Icon(Icons.error),
-                          ),
-                        ),
                         Text(
                           auctionItemModelList[index].itemName ?? "",
-                          style: TextStyle(
-                            fontSize: 20.sp,
-                            color: AppColors.primaryBlack,
-                            fontWeight: FontWeight.w600,
-                          ),
+                          style: TextStyle(fontSize: 20.sp),
                         ),
-
                         Text(
                           auctionItemModelList[index].description ?? "",
-                          style: TextStyle(fontSize: 13.sp),
-                        ),
-                        SizedBox(height: 5.h),
-                        Text(
-                          "Highest bid £${auctionItemModelList[index].highestBid.toString()}",
                           style: TextStyle(
                             fontSize: 13.sp,
-                            fontWeight: FontWeight.w600,
+                            overflow: TextOverflow.ellipsis,
+                            color: AppColors.primaryBlack.withAlpha(
+                              (255 * .8).round(),
+                            ),
                           ),
+                          maxLines: 2,
                         ),
                         SizedBox(height: 6.h),
-                        Row(
-                          children: [
-                            SvgPicture.asset(AppAssets.timerIcon, width: 15.sp),
-                            SizedBox(width: 6.w),
-                            Text(
-                              "${auctionItemModelList[index].timeLeft} left",
+
+                        Text("Color: Black"),
+                        Text("Size: XL"),
+                        Text("Qty: 1"),
+
+                        SizedBox(height: 6.h),
+
+                        Text(
+                          "00d:05h:22 sec left",
+                          style: TextStyle(color: AppColors.primaryDanger),
+                        ),
+                        Text("£420.00 Bids", style: TextStyle(fontSize: 15.sp)),
+                        InkWell(
+                          onTap: () {},
+                          child: Text(
+                            "Remove",
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 15.sp,
                             ),
-                          ],
+                          ),
                         ),
                       ],
                     ),
-
-                    Positioned(
-                      top: 5,
-                      right: 5,
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.favorite,
-                          color: AppColors.primaryDanger,
-                        ),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
+              ],
             ),
           ),
           separatorBuilder: (context, index) => SizedBox(height: 0.h),
